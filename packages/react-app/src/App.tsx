84312146a91537c2efe80feefd76c81bbab71a98
Naufan <naufan.rizal@shuttlerock.com>
2021-09-01 13:46:40 +1200

    Add eslint config

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {someFunction} from '@monorepo/library';
+// import {someFunction} from '../../library'; // fail
 
 function App() {
   return (
